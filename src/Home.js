5224cfae158adb0c402041f3bfd7dbf31dfebb15
Surya Teja <pratapreddy0808@gmail.com>
2023-02-26 23:37:55 +0530

    Added mobile responsiveness

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import SearchComponent from "./SearchComponent";
 import ListCoins from './ListCoins';
 import './app.css'
 
 export default function Home( { coins , filteredCoins , setFilteredCoins , isLoading , fetchError , refresh } ) {
-
+  const [windowWidth , setWindowWidth] = useState(window.innerWidth)
+  window.addEventListener("resize", function () {setWindowWidth(windowWidth.innerWidth)});
   return (
-    <div className="container w-75 text-center text-white home">
+    <div className={`container-fluid ${windowWidth < 576 ? 'w-100' : 'w-75'} text-center text-white home`}>
       <div className="row justify-content-center">
         <div className="col-12 h1 mt-5"><u>Welcome to the CryptoChecker</u></div>
         <div className="col-12 ">
           <SearchComponent refresh={refresh} setFilteredCoins={setFilteredCoins} />
         </div>
         <div className="col-12 px-0">
-          <ListCoins coins={coins} filteredCoins={filteredCoins} isLoading={isLoading} fetchError={fetchError} />
+          <ListCoins windowWidth={windowWidth} coins={coins} filteredCoins={filteredCoins} isLoading={isLoading} fetchError={fetchError} />
         </div>
       </div>
     </div>
--- src/ListCoins.js
+++ src/ListCoins.js
@@ -2,23 +2,21 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Loader from './Loader';
 
-export default function ListCoins( { coins , filteredCoins , isLoading , fetchError } ) {
-  const [windowWidth , setWindowWidth] = useState(window.innerWidth)
-  window.addEventListener("resize", function () {setWindowWidth(windowWidth.innerWidth)});
+export default function ListCoins( { windowWidth , coins , filteredCoins , isLoading , fetchError } ) {
 
   return (
     <>
       {isLoading && <Loader />}
       {fetchError && <p className='h3 mt-5 pt-5 text-danger'>{fetchError}</p>}
-      {!isLoading && !fetchError && <table className='table text-start text-bg-dark m-auto table-responsive-md'>
+      {!isLoading && !fetchError && <table className={`table text-start text-bg-dark m-auto ${windowWidth<=768 && "m-0"}`}>
         <tbody>
           {coins.filter(coin => coin.name.toLowerCase().includes(filteredCoins.toLowerCase())).map( coin => {
             return <tr className='align-middle' key={coin.id}>
-              <td><img src={coin.image} alt={`${coin.name} icon`} /></td>
-              <td>{coin.name}</td>
-              <td>{coin.symbol}</td>
+              <td><img src={coin.image} alt={`${coin.name} icon`} style={windowWidth < 576 ? {"width" : "40px"} : {"width" : "55px"}} /></td>
+              <td style={windowWidth < 576 && {"padding-right" : "0"}}>{coin.name}</td>
+              <td className={windowWidth <= 768 && 'd-none'}>{coin.symbol}</td>
               <td>{`₹${coin.current_price.toLocaleString()}`}</td>
-              <td className={coin.price_change_percentage_24h >= 0 ? 'text-success' : 'text-danger'}>{coin.price_change_percentage_24h.toFixed(2)}</td>
+              <td className={`${coin.price_change_percentage_24h >= 0 ? 'text-success' : 'text-danger'} ${windowWidth <= 768 && 'd-none'}`}>{coin.price_change_percentage_24h.toFixed(2)}</td>
               <td className={windowWidth <= 768 && 'd-none'}>₹{coin.market_cap.toLocaleString()}</td>
               <td><Link className='btn btn-primary text-nowrap' to={`/${coin.id}`} >More info</Link></td>
             </tr>
