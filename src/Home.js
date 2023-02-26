7a695a5aa9c72a13adeb2a73cf6df0cd8b3a9f87
Surya Teja <pratapreddy0808@gmail.com>
2023-02-27 00:37:39 +0530

    Added mobile responsiveness

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,15 @@ import './app.css'
 
 export default function Home( { coins , filteredCoins , setFilteredCoins , isLoading , fetchError , refresh } ) {
   const [windowWidth , setWindowWidth] = useState(window.innerWidth)
-  window.addEventListener("resize", function () {setWindowWidth(windowWidth.innerWidth)});
+  window.addEventListener("resize", handleResize );
+
+  function handleResize() {
+    setWindowWidth(window.innerWidth)
+  }
   return (
-    <div className={`container ${windowWidth < 576 ? 'w-100' : 'w-75'} text-center text-white home`}>
+    <div className={`container ${windowWidth <= 769 ? 'w-100' : 'w-75'} text-center text-white home`}>
       <div className="row justify-content-center">
-        <div className="col-12 h1 mt-5"><u>Welcome to the CryptoChecker</u></div>
+        <div className="col-12 h1 mt-5"><u>Welcome to the CryptoTracker</u></div>
         <div className="col-12 ">
           <SearchComponent refresh={refresh} setFilteredCoins={setFilteredCoins} />
         </div>
